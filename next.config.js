be043c7e9c27b744a3eed881626669812808f247
developer <150611229+fanlyai@users.noreply.github.com>
2023-11-19 22:26:36 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   i18n: {
-    
+    localeDetection: false,
     locales: ['en', 'zh', 'ja', 'es', 'hi', 'tr'],
     defaultLocale: 'en',
     localeDetection: false,
   },
-  trailingSlash: true,
+  
   reactStrictMode:true,
 }
